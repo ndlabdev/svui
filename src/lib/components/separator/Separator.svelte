2d979901ad78fe1e46b3de6e491f98d55ec37069
Đăng Long Nguyễn <nguyenlongdang04121999@gmail.com>
2025-05-07 16:39:53 +0700

    feat(separator): add flexible Separator component with full variant support

---
+++ src/lib/components/separator/Separator.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+    import { separatorTheme, type SeparatorProps } from '.'
+
+    const {
+        color,
+        size,
+        type,
+        label,
+        icon,
+        orientation = 'horizontal',
+        class: className,
+        ui,
+        ...restProps
+    }: SeparatorProps = $props()
+
+    const uiSeparator = $derived(separatorTheme({
+        color,
+        orientation,
+        size,
+        type
+    }))
+
+    const uiRoot = $derived(uiSeparator.root({
+        class: [className?.toString(), ui?.root]
+    }))
+
+    const uiBorder = $derived(uiSeparator.border({
+        class: ui?.border
+    }))
+
+    const uiContainer = $derived(uiSeparator.container({
+        class: ui?.container
+    }))
+
+    const uiLabel = $derived(uiSeparator.label({
+        class: ui?.label
+    }))
+
+    const uiIcon = $derived(uiSeparator.icon({
+        class: ui?.icon
+    }))
+</script>
+
+<div class={uiRoot} {...restProps}>
+    <div class={uiBorder}></div>
+
+    {#if label || icon}
+        <div class={uiContainer}>
+            {#if label}
+                <span class={uiLabel}>{label}</span>
+            {:else if icon}
+                {@const Icon = icon}
+                <Icon class={uiIcon} />
+            {/if}
+        </div>
+
+        <div class={uiBorder}></div>
+    {/if}
+</div>
