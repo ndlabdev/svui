cdb5e9ea5cd1fff7b6e71b7aabd95d1a85006fe1
Đăng Long Nguyễn <nguyenlongdang04121999@gmail.com>
2025-05-08 14:49:18 +0700

    feature skeleton

---
+++ src/lib/components/skeleton/Skeleton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { skeletonTheme, type SkeletonProps } from '.'
+
+    const {
+        as = 'div',
+        class: className
+    }: SkeletonProps = $props()
+
+    const uiSkeleton = $derived(skeletonTheme({
+        class: className?.toString()
+    }))
+</script>
+
+<svelte:element
+    this={as}
+    class={uiSkeleton}
+    aria-busy="true"
+    aria-label="loading"
+    aria-live="polite"
+    role="alert"
+/>
---
+++ src/lib/components/skeleton/theme.ts
@@ -0,0 +1,5 @@
+import { tv } from 'tailwind-variants'
+
+export const skeletonTheme = tv({
+    base: 'animate-pulse rounded-md bg-secondary-100'
+})
