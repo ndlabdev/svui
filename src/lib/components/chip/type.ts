d29844e2bd156fe0e1ff3040383a24b37d63331d
Nguyen Long Dang <74055192+ndlabdev@users.noreply.github.com>
2025-05-14 08:55:23 +0700

    Merge pull request #15
    * Custom Colors generate
    
    * Fix Steps 1 Button and Eslint
    
    * Eslint setup
    
    * turn off import/no-unresolve
    
    * test link components
    
    * Optimize Code feature button
    
    * Leading icon button
    
    * optimize theme and button generate manually
    
    * fix multiple dev with concurrently
    
    * remove colors safelist
    
    * dynamic button
    
    * feat(ui): add Button component with support for variants, sizes, and …
    
    * comment docs button
    
    * feat(ui): add Button component with support for variants, sizes, and …
    
    * Typography heading
    
    * Merge branch 'feature/typography' into dev
    
    * Merge branch 'main' of https://github.com/ndlabdev/svui into dev
    
    * feat(separator): add flexible Separator component with full variant s…
    
    * Merge branch 'feature/separator' into dev
    
    * Merge branch 'main' of https://github.com/ndlabdev/svui into dev
    
    * Card coming soon
    
    * Merge branch 'feature/card' into dev
    
    * Merge branch 'main' of https://github.com/ndlabdev/svui into dev
    
    * Alert component
    
    * Alert component add actions
    
    * Merge branch 'feature/alert' into dev
    
    * Merge branch 'main' of https://github.com/ndlabdev/svui into dev
    
    * feature skeleton (#6)
    
    * Merge branch 'feature/skeleton' into dev
    
    * Merge branch 'main' into dev
    
    * feature/config (#10)
    
    * Merge branch 'main' into dev
    
    * Feature/accordion
    
    * accordion fix force mount animation
    
    * Accordion slot components
    
    * Merge branch 'feature/accordion' into dev
    
    * Avatar and apply into button component
    
    * feature/avatar
    
    * Merge branch 'feature/avatar' into dev
    
    * feature/chip
    
    * Merge remote-tracking branch 'origin/feature/chip' into dev

---
+++ src/lib/components/chip/type.ts
@@ -0,0 +1,18 @@
+import type { Snippet } from 'svelte'
+import type { HTMLAttributes } from 'svelte/elements'
+import type { chipTheme } from './theme'
+
+export interface ChipSlots {
+	slotContent?: Snippet
+}
+
+export interface ChipProps extends HTMLAttributes<HTMLDivElement>, ChipSlots {
+	as?: keyof HTMLElementTagNameMap
+	text?: string | number
+	color?: keyof typeof chipTheme['variants']['color']
+	size?: keyof typeof chipTheme['variants']['size']
+	position?: keyof typeof chipTheme['variants']['position']
+	inset?: boolean
+	standalone?: boolean
+	ui?: Partial<typeof chipTheme['slots']>
+}
