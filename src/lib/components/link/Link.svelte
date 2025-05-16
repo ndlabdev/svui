45fe50a4bf7980eb950d4a47e960a5c5566d3df2
nguyenlongdang0412 <nguyenlongdang04121999@gmail.com>
2025-05-16 23:21:01 +0700

    use query hash

--- src/lib/components/link/Link.svelte
+++ src/lib/components/link/Link.svelte
@@ -3,7 +3,8 @@
     import { LinkBase, type LinkProps, linkTheme } from '.'
     import uiConfig from '#uiconfig'
     import { page } from '$app/state'
-    import { browser } from '$app/environment';
+    import { browser } from '$app/environment'
+    import { isEqual, diff } from 'ohash/utils'
 
     const {
         children,
@@ -45,9 +46,35 @@
 
     const url = $derived(new URL(href as string, browser ? window.location.href : 'http://localhost'))
 
+    function useQuery(query: any) {
+        return Object.fromEntries(
+          [...query.entries()].map(([key, value]) => [key, value])
+        )
+    }
+
+    function isPartiallyEqual(item1: any, item2: any) {
+        const diffedKeys = diff(item1, item2).reduce((filtered, q) => {
+            if (q.type === 'added') {
+                filtered.add(q.key)
+            }
+            return filtered
+        }, new Set<string>())
+
+        const item1Filtered = Object.fromEntries(Object.entries(item1).filter(([key]) => !diffedKeys.has(key)))
+        const item2Filtered = Object.fromEntries(Object.entries(item2).filter(([key]) => !diffedKeys.has(key)))
+
+        return isEqual(item1Filtered, item2Filtered)
+    }
+
     function isLinkActive() {
         if (active !== undefined) {
             return active
+        }
+
+        if (exactQuery === 'partial') {
+            if (!isPartiallyEqual(useQuery(page.url.searchParams), useQuery(url.searchParams))) return false
+        } else if (exactQuery === true) {
+            if (!isEqual(useQuery(page.url.searchParams), useQuery(url.searchParams))) return false
         }
 
         if (exactHash && url.hash !== page.url.hash) {
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,11 +4,11 @@
 
 <div class="grid grid-cols-12">
     <div class="col-span-12">
-        <Link href="/" exactHash raw class="text-2xl" activeClass="font-bold" inactiveClass="text-muted">
+        <Link href="/" exactQuery raw class="text-2xl" activeClass="font-bold" inactiveClass="text-muted">
             Home
         </Link>
 
-        <Link href="/about" exactHash raw class="text-2xl" activeClass="font-bold" inactiveClass="text-muted">
+        <Link href="/about?type=1&search=2" exactQuery raw class="text-2xl" activeClass="font-bold" inactiveClass="text-muted">
             About
         </Link>
     </div>
